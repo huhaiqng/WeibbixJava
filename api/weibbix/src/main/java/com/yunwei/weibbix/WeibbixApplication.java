982188a53d2a2f92bfbb09ed8000ba81a2f0e98a
unknown <1321385590@qq.com>
2018-12-18 11:16:57 +0800

    commit

---
+++ api/weibbix/src/main/java/com/yunwei/weibbix/WeibbixApplication.java
@@ -0,0 +1,14 @@
+package com.yunwei.weibbix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeibbixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeibbixApplication.class, args);
+	}
+
+}
+
