95a5d3c07eb87ca2833c8b7863a53f4903735adc
unknown <1321385590@qq.com>
2018-12-21 11:52:45 +0800

    commit

--- static/js/user.js
+++ static/js/user.js
@@ -1,10 +1,10 @@
 $(function () {
-	$("#usermanage").click(function(){
+	$("#createUser").click(function(){
+		console.log("hello js");
 		$("#indexcontent").load("user.html");
 		var script = document.createElement('script');
 		script.async = false;
 		script.setAttribute('src', 'js/user.js'); 
 		document.body.appendChild(script);
     });
-	
-}); 
+});
